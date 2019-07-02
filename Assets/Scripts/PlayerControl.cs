2f5ab4978f499f84a836bc86850517db69482588
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-07-02 21:29:00 +0300

    Show health on UI, decrement health on collision with pumpkin

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
   CharacterController characterController;
   Animator animator;
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+  public int maxHealth = 6;
+  public int startHealth = 3;
+
+  public GameObject ui;
+  public GameObject healthIcon;
+
+  private List<GameObject> healthIcons = new List<GameObject>();
+
+  private int health = 0;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    health = startHealth;
+    updateUIIcons();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.name == "GhostWrapper")
+    {
+      Debug.Log("Collision with ghost");
+      if (health > 0)
+      {
+        health--;
+        updateUIIcons();
+      }
+
+      Destroy(other.gameObject);
+    }
+  }
+
+  void updateUIIcons()
+  {
+    while (healthIcons.Count < health)
+    {
+      GameObject icon = Instantiate(healthIcon);
+      icon.transform.SetParent(ui.transform, false);
+      healthIcons.Add(icon);
+      icon.transform.position = new Vector3(120 * healthIcons.Count, 100, 0);
+    }
+    while (healthIcons.Count > health)
+    {
+      GameObject o = healthIcons[healthIcons.Count - 1];
+      healthIcons.Remove(o);
+      Destroy(o);
+    }
+  }
+}
