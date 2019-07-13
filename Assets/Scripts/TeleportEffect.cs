1c4f0df1ae06d49e05c5e1631f44bc16c97d3659
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-07-13 15:55:03 +0300

    Allow teleporting

--- Assets/Scripts/TeleportEffect.cs
+++ Assets/Scripts/TeleportEffect.cs
@@ -9,39 +9,65 @@ public class TeleportEffect : MonoBehaviour
   public AnimationCurve fadeIn;
 
   ParticleSystem ps;
-  float timer = 0;
-  Renderer[] _renderers;
+  float solidity = 1;
+
+  Renderer[] renderers;
 
   int shaderProperty;
 
+  public static bool teleporting = false;
+  Vector3 teleportTarget = new Vector3(0, 0, 0);
+
+  GameObject player;
+  CharacterController controller;
+
   void Start()
   {
+    player = GameObject.Find("Player");
+    controller = player.GetComponent<CharacterController>();
     shaderProperty = Shader.PropertyToID("_cutoff");
-    _renderers = GetComponentsInChildren<Renderer>();
+    renderers = GetComponentsInChildren<Renderer>();
     ps = GetComponentInChildren<ParticleSystem>();
 
     var main = ps.main;
     main.duration = spawnEffectTime;
 
     ps.Play();
-
   }
 
   void Update()
   {
-    if (timer < spawnEffectTime + pause)
-    {
-      timer += Time.deltaTime;
-    }
-    else
-    {
-      ps.Play();
-      timer = 0;
+    if (teleporting) {
+      if (solidity == 1) {
+        ps.Play();
+      }
+      if (solidity > 0) {
+        solidity = Mathf.Max(solidity - Time.deltaTime / spawnEffectTime, 0);
+      }
+      if (solidity == 0) {
+        teleportPlayer();
+      }
+    } else {
+      if (solidity < 1) {
+        solidity = Mathf.Min(solidity + Time.deltaTime / spawnEffectTime, 1);
+      }
+      if (solidity == 1) {
+        ps.Stop();
+      }
     }
 
-    float cutoffValue = fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer));
-    for (int i = 0; i < _renderers.Length; ++i) {
-      _renderers[i].material.SetFloat(shaderProperty, cutoffValue);
+    float cutoffValue = fadeIn.Evaluate(solidity);
+    for (int i = 0; i < renderers.Length; ++i) {
+      renderers[i].material.SetFloat(shaderProperty, cutoffValue);
     }
+  }
+
+  void teleportPlayer() {
+    Debug.Log("Teleporting player to " + teleportTarget);
+    teleporting = false;
+    controller.enabled = false;
+    player.transform.position = teleportTarget;
+    controller.enabled = true; 
+    ps.Play();
   }
 }
