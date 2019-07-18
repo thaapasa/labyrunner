dedaf7023d73b065199919e98480750153771e87
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-07-18 23:05:40 +0300

    Make ghosts that move differently

--- Assets/Scripts/GhostMover.cs
+++ Assets/Scripts/GhostMover.cs
@@ -13,6 +13,8 @@ public class GhostMover : MonoBehaviour
 
   private CreateLevel levelScript;
 
+  private MoveStrategy strategy;
+
   private Vector3[] moveVectors = new Vector3[] {
         new Vector3(0, 0, -1),
         new Vector3(-1, 0, 0),
@@ -23,8 +25,17 @@ public class GhostMover : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    strategy = createMoveStrategy();
     level = GameObject.Find("Level");
     levelScript = level.gameObject.GetComponent<CreateLevel>();
+  }
+
+  private MoveStrategy createMoveStrategy()
+  {
+    var f = Random.Range(0, 1.15f);
+    if (f > 1) { return new RandomTurnStrategy(); }
+    else if (f > 0.5) { return new TurnLeftStrategy(); }
+    else { return new TurnRightStrategy(); }
   }
 
   private void newTarget()
@@ -43,30 +54,9 @@ public class GhostMover : MonoBehaviour
       return;
     }
 
-    direction = selectNewDirection(lab, pos, direction);
+    direction = strategy.selectNewDirection(lab, pos, direction);
     moveLeft = wallWidth;
     // Debug.Log("Ghost moves " + direction);
-  }
-
-  private Direction selectNewDirection(Labyrinth lab, Coordinates pos, Direction dir)
-  {
-    Direction right = DirectionHelper.turnRight(dir);
-    if (!lab.hasWallTo(pos.x, pos.y, right)) {
-      return right;
-    }
-    // Debug.Log("Could not turn right from " + pos + " towards " + right);
-    if (!lab.hasWallTo(pos.x, pos.y, dir)) {
-      return dir;
-    }
-    // Debug.Log("Could not move straight from " + pos + " towards " + dir);
-    Direction left = DirectionHelper.turnLeft(dir);
-    if (!lab.hasWallTo(pos.x, pos.y, left)) {
-      return left;
-    }
-    // Debug.Log("Could not turn left from " + pos + " towards " + left);
-    dir = DirectionHelper.turnRight(right);
-    // Debug.Log("Returning towards " + dir + ", wall: " + lab.hasWallTo(pos.x, pos.y, dir));
-    return dir;
   }
 
   void FixedUpdate()
@@ -90,5 +80,92 @@ public class GhostMover : MonoBehaviour
     transform.rotation.SetLookRotation(moveVectors[(int)direction]);
     moveLeft -= moveNow;
 
+  }
+}
+
+interface MoveStrategy
+{
+  Direction selectNewDirection(Labyrinth lab, Coordinates pos, Direction dir);
+}
+
+class TurnRightStrategy : MoveStrategy
+{
+
+  public Direction selectNewDirection(Labyrinth lab, Coordinates pos, Direction dir)
+  {
+    Direction right = DirectionHelper.turnRight(dir);
+    if (!lab.hasWallTo(pos.x, pos.y, right))
+    {
+      return right;
+    }
+    // Debug.Log("Could not turn right from " + pos + " towards " + right);
+    if (!lab.hasWallTo(pos.x, pos.y, dir))
+    {
+      return dir;
+    }
+    // Debug.Log("Could not move straight from " + pos + " towards " + dir);
+    Direction left = DirectionHelper.turnLeft(dir);
+    if (!lab.hasWallTo(pos.x, pos.y, left))
+    {
+      return left;
+    }
+    // Debug.Log("Could not turn left from " + pos + " towards " + left);
+    dir = DirectionHelper.turnRight(right);
+    // Debug.Log("Returning towards " + dir + ", wall: " + lab.hasWallTo(pos.x, pos.y, dir));
+    return dir;
+  }
+}
+
+class TurnLeftStrategy : MoveStrategy
+{
+
+  public Direction selectNewDirection(Labyrinth lab, Coordinates pos, Direction dir)
+  {
+    Direction left = DirectionHelper.turnLeft(dir);
+    if (!lab.hasWallTo(pos.x, pos.y, left))
+    {
+      return left;
+    }
+    // Debug.Log("Could not turn right from " + pos + " towards " + right);
+    if (!lab.hasWallTo(pos.x, pos.y, dir))
+    {
+      return dir;
+    }
+    // Debug.Log("Could not move straight from " + pos + " towards " + dir);
+    Direction right = DirectionHelper.turnRight(dir);
+    if (!lab.hasWallTo(pos.x, pos.y, right))
+    {
+      return right;
+    }
+    // Debug.Log("Could not turn left from " + pos + " towards " + left);
+    dir = DirectionHelper.turnRight(right);
+    // Debug.Log("Returning towards " + dir + ", wall: " + lab.hasWallTo(pos.x, pos.y, dir));
+    return dir;
+  }
+}
+
+class RandomTurnStrategy : MoveStrategy
+{
+
+  public Direction selectNewDirection(Labyrinth lab, Coordinates pos, Direction dir)
+  {
+    List<Direction> possibilities = new List<Direction>();
+    if (!lab.hasWallTo(pos.x, pos.y, Direction.NORTH))
+    {
+      possibilities.Add(Direction.NORTH);
+    }
+    if (!lab.hasWallTo(pos.x, pos.y, Direction.SOUTH))
+    {
+      possibilities.Add(Direction.SOUTH);
+    }
+    if (!lab.hasWallTo(pos.x, pos.y, Direction.EAST))
+    {
+      possibilities.Add(Direction.EAST);
+    }
+    if (!lab.hasWallTo(pos.x, pos.y, Direction.WEST))
+    {
+      possibilities.Add(Direction.WEST);
+    }
+    return possibilities[Random.Range(0, possibilities.Count)];
   }
 }
